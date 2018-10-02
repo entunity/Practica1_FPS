d0856b0386ca6a98eaff1a196f0af90ed4cd9e07
entunity <enrique.tunity@gmail.com>
2018-10-02 20:24:57 +0200

    02/10/2018
    Enemigo simple con  script de movimiento

--- Assets/_GameAssets/Scripts/EnemigoSimple.cs
+++ Assets/_GameAssets/Scripts/EnemigoSimple.cs
@@ -2,15 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemigoSimple : MonoBehaviour {
+public class EnemigoSimple : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] private float velocidad = 1.5f;
+    [SerializeField] float tiempocambiodireccion = 2;
+
+
+    private Vector3 direccion;
+    private float timer;
+    // Use this for initialization
+    void Start()
+    {
+        direccion = new Vector3(Random.Range(-1, 2), 0, Random.Range(-1, 2));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > tiempocambiodireccion)
+        {
+            direccion = new Vector3(Random.Range(-1, 2), 0, Random.Range(-1, 2));
+            timer = 0;
+        }
+        transform.Translate(direccion * velocidad * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if ((collision.gameObject.tag == Configuracion.capaMunicion))
+        {
+            Destroy(this.gameObject);
+        }
+    }
 }
