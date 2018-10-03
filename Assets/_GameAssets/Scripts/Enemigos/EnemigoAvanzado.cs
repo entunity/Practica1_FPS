c0733bc9c794c144764f58b85939eaaea360d75b
entunity <enrique.tunity@gmail.com>
2018-10-03 22:06:01 +0200

    03/10/2018

---
+++ Assets/_GameAssets/Scripts/Enemigos/EnemigoAvanzado.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemigoAvanzado : MonoBehaviour {
+
+    [SerializeField] private float velocidad = 1.5f;
+    [SerializeField] float tiempocambiodireccion = 2;
+
+
+    public Vector3 direccion;
+    private float timer;
+    // Use this for initialization
+    void Start()
+    {
+        direccion = new Vector3(Random.Range(-1, 2), 0, Random.Range(-1, 2));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > tiempocambiodireccion)
+        {
+            direccion = new Vector3(Random.Range(-1, 2), 0, Random.Range(-1, 2));
+            timer = 0;
+        }
+        transform.Translate(direccion * velocidad * Time.deltaTime);
+    }
+
+
+}
---
+++ Assets/_GameAssets/Scripts/Enemigos/NucleoEnemAvanz.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NucleoEnemAvanz : MonoBehaviour {
+
+
+    [SerializeField] GameObject padre;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if ((collision.gameObject.tag == Configuracion.capaMunicion))
+        {
+            Destroy(padre.gameObject);
+        }
+    }
+}
